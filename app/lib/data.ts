6d5c5915c0ffc5b8b4baf3f9875cc53ef99d06aa
roland sokel <rldsokel48@gmail.com>
2024-05-27 18:01:20 +0000

    18h01

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -20,7 +20,6 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -42,7 +41,7 @@ export async function fetchLatestInvoices() {
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
-
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
